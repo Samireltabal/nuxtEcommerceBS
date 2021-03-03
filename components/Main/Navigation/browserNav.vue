d6dab9b4a819c2d5aa099aa6dcb090e47371b6bc
Samir eltabal <samir.m.eltabal@gmail.com>
2021-03-03 20:38:05 +0200

    update navigation

---
+++ components/Main/Navigation/browserNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-navbar type="light" variant="light">
+      <b-navbar-nav>
+        <b-navbar-brand href="#">
+          NavBar
+        </b-navbar-brand>
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Lang" left>
+          <b-dropdown-item
+            v-for="locale in langs"
+            :key="locale.code"
+            :to="switchLocalePath(locale.code)"
+          >
+            {{ locale.name }}
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="User" left>
+          <b-dropdown-item href="#">
+            Account
+          </b-dropdown-item>
+          <b-dropdown-item href="#">
+            Settings
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+<script>
+export default {
+  name: 'BroswerNav',
+  props: {
+    langs: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/Main/navBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <browserNav :langs="availableLocales" />
+  </div>
+</template>
+<script>
+import browserNav from '@/components/Main/Navigation/browserNav'
+export default {
+  components: {
+    browserNav
+  },
+  computed: {
+    availableLocales () {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+    },
+    currentLocale () {
+      return this.$i18n.locale
+    }
+  }
+}
+</script>
