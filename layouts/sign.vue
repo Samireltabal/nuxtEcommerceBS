83507036a94a3b1d3f4642849e7c25d00be5a196
Samir M. Eltabal <samir.m.eltabal@gmail.com>
2021-03-11 14:45:17 +0200

    init login page

---
+++ layouts/sign.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="container">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  head () {
+    return {
+      title: 'awesome title',
+      htmlAttrs: {
+        lang: this.currentLocale,
+        dir: this.direction
+      }
+    }
+  },
+  computed: {
+    availableLocales () {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+    },
+    currentLocale () {
+      return this.$i18n.locale
+    },
+    direction () {
+      if (this.currentLocale === 'ar') {
+        return 'rtl'
+      } else {
+        return 'ltr'
+      }
+    }
+  }
+}
+</script>
+<style>
+
+</style>
